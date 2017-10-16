46b81314c2f26e0cc8d493d25cb4d35f3db66d29
Alan Hong <xiupan@gmail.com>
2017-10-16 11:56:47 -0500

    Problem 1 solved!

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Project Euler
 
 This repo serves as a log of my attempts to solve Project Euler problems with Javascript (and maybe some other languages!). It's mission: to prepare myself for tech interviews.
+
+## TODO:
+- Add testing library with npm/yarn?
--- set01/problem-001.js
+++ set01/problem-001.js
@@ -1,7 +1,17 @@
 // Find the sum of all multiples of 3 and 5 below given number
 
 const mult3and5 = (maxNum) => {
-  console.log(maxNum);
+  console.log(`Input number is: ${maxNum}`)
+  let sum = 0
+
+  for (let i = 1; i < maxNum; ++i) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      // console.log(`Multiple of 3 or 5 found: ${i}`);
+      sum += i
+    }
+  }
+  console.log(`The sum of all multiples of 3 and 5 below ${maxNum} is ${sum}.`);
 }
 
-mult3and5(1000)
+mult3and5(10) // should equal 23
+mult3and5(1000) // the answer to problem 1
